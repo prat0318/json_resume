a150f8545fd2734a2df83e019bc8808f3e848d5b
Prateek Agarwal <prat0318@gmail.com>
2014-02-03 21:55:36 -0600

    Initial reader.rb which takes in json file path or json string and reads it

---
+++ lib/json_resume/reader.rb
@@ -0,0 +1,17 @@
+module JsonResume
+	class << self
+		def read_json_file(name)
+
+
+		end
+
+		def read_json_string(json_string)
+      puts "ii ii"
+
+		end
+    
+    def read(string)
+
+
+    end
+
