6f39e4fc1e29adde1cac7bf5eeb65164f8a2713e
Prateek Agarwal <prat0318@gmail.com>
2014-04-20 16:11:18 -0500

    Format linkedin and github url

--- lib/json_resume/formatter_latex.rb
+++ lib/json_resume/formatter_latex.rb
@@ -3,6 +3,11 @@ require_relative 'formatter'
 module JsonResume
   class FormatterLatex < Formatter
 
+    def format_linkedin_github_url
+     @hash["linkedin_url"].gsub! /.*\/(.*?)\/?$/, '\1' if @hash["linkedin_url"] 
+     @hash["github_url"].gsub! /.*\/(.*?)\/?$/, '\1' if @hash["github_url"] 
+    end
+
     def format_link! str
         str.gsub! /\[(.*?)\]{(.*?)}/, '{\color{see} \href{\2}{\1}}'
     end
@@ -14,6 +19,14 @@ module JsonResume
     def format_string! str
       format_link! str
       format_autolink! str
+    end
+
+    def format
+      super
+
+      format_linkedin_github_url
+
+      self
     end
   end
 end
