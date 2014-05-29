aa3a3c4001fc1ed5ac108a5d007b325c7fc22828
Prateek Agarwal <prat0318@gmail.com>
2014-05-28 23:40:41 -0700

    Make html convert working

--- lib/json_resume.rb
+++ lib/json_resume.rb
@@ -1,5 +1,5 @@
-require "json_resume/version"
-require "json_resume/json_resume"
+require_relative "json_resume/version"
+require_relative "json_resume/json_resume"
 
 module JsonResume
   # Your code goes here...
