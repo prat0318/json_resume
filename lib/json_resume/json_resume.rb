58b9bab429d0d42c66b9077f2cd9fb1edd4266c2
Prateek Agarwal <prat0318@gmail.com>
2014-07-01 23:59:29 -0700

    Correct formatting

--- lib/json_resume/json_resume.rb
+++ lib/json_resume/json_resume.rb
@@ -3,14 +3,14 @@ require_relative 'reader'
 module JsonResume
 	class << self
 		def new(json_input, options = {})
-      options = options.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
+			options = options.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
 			JsonResume::Core.new(json_input, options)
 		end
 	end
 
 	class Core
 		attr_accessor :reader
-		
+
 		def initialize(json_input, options)
 			@reader = Reader.new(json_input, options)
 			@reader.format!
